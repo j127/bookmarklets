80fc20cf2d108732c5acef1d6136e79f60780d18
J. Cohen <hostelmarketing@gmail.com>
2015-04-19 23:00:33 -0700

    Bugfix

--- hide_interstitials/hideInterstitials.js
+++ hide_interstitials/hideInterstitials.js
@@ -5,6 +5,6 @@
     if (currentHost.indexOf('www.quora.com') > -1) {
         var $x = $('.modal_signup_background'), $y = $x.next(); $x.remove(); $y.remove();
     } else if (currentHost.indexOf('www.pinterest.com') > -1) {
-        $('.UnauthBanner').remove();
+        $('.Modal').remove();
     }
-})();
+}());
