86751b789c16ddd25472a3db004f69da6890ac71
J. Cohen <hostelmarketing@gmail.com>
2015-04-19 23:44:07 -0700

    Format code

--- hide_interstitials/hideInterstitials.js
+++ hide_interstitials/hideInterstitials.js
@@ -3,7 +3,11 @@
     var currentHost = window.location.host;
 
     if (currentHost.indexOf('www.quora.com') > -1) {
-        var $x = $('.modal_signup_background'), $y = $x.next(); $x.remove(); $y.remove();
+        var $x = $('.modal_signup_background'),
+            $y = $x.next();
+
+            $x.remove();
+            $y.remove();
     } else if (currentHost.indexOf('www.pinterest.com') > -1) {
         $('.Modal').remove();
     }
