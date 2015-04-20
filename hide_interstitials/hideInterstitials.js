59917ea58f05ad7d8932501773324a35d1f540f9
J. Cohen <hostelmarketing@gmail.com>
2015-04-19 23:45:38 -0700

    Fix spelling error

--- hide_interstitials/hideInterstitials.js
+++ hide_interstitials/hideInterstitials.js
@@ -1,5 +1,5 @@
 (function () {
-    // Hides intersitital signin forms on Quora and Pinterest
+    // Hides interstitial signin forms on Quora and Pinterest
     var currentHost = window.location.host;
 
     if (currentHost.indexOf('www.quora.com') > -1) {
